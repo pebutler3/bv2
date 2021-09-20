26f3f25d142460e54819395bbd113ab92437e19b
Paul Butler III <paul@heysparkbox.com>
2021-09-20 01:58:36 -0600

    feat: create RichText component

---
+++ src/components/RichText.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="c-rich-text">
+    <div class="c-rich-text__html" v-html="text" v-if="isTypeHtml"/>
+    <p v-else>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RichText',
+  props: {
+    type: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    text: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+  computed: {
+    isTypeHtml() {
+      return this.type === 'html';
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
