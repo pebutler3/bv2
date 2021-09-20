a31f0c4a022d4d08cea1fbc4750a3f1f50888494
Paul Butler III <paul@heysparkbox.com>
2021-09-20 01:58:51 -0600

    feat: create Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="c-title">
+    <div v-html="text" :class="classComputed" v-if="textType === 'html'" />
+    <p v-else>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    textType: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    className: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    styleModifier: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  computed: {
+    classComputed() {
+      return this.styleModifier
+        ? `c-title__${this.className} ${this.styleModifier}`
+        : `c-title__${this.className}`;
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
