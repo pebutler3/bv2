00362f4c56f7d75093229c332d9181a6e6a58db2
Paul Butler III <paul@heysparkbox.com>
2021-09-20 01:57:12 -0600

    feat: add global styles
    - set routes from store

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" v-if="routes">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/bios">Bios</router-link> |
+      <router-link to="/books">Books</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+  async created() {
+    await this.getRoutes();
+
+    if (this.routes) {
+      await this.getBios();
+      await this.getBooks();
+    } else {
+      this.$router.push('/maintenance');
+    }
+  },
+  computed: {
+    ...mapState({ routes: state => state.routes }),
+  },
+  methods: {
+    ...mapActions(['getRoutes', 'getBios', 'getBooks']),
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,11 +46,22 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
 
     &.router-link-exact-active {
       color: #42b983;
     }
   }
+}
+
+.uppercase {
+  text-transform: uppercase;
+}
+
+ul {
+  list-style: none;
+}
+
+.order-list {
+  display: flex;
 }
 </style>
