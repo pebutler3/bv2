4737934819cd0fdf0a7da473bf9761d85ed39615
Paul Butler III <paul@heysparkbox.com>
2021-09-20 01:57:54 -0600

    feat: create Container component

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="c-container" :style="`flex-direction: ${flexDirection}`">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+    flexDirection: {
+      type: String,
+      default: 'row',
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.c-container {
+  display: flex;
+}
+</style>
