5ae3af6f52df2b85b9ba5778b6e1b3b0aa56d2e4
Paul Butler III <paul@heysparkbox.com>
2021-09-20 02:01:22 -0600

    feat: add base routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter)
 
@@ -11,19 +11,34 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/*/:id',
+    name: 'ID',
+    component: () => import('../views/[id].vue'),
+    params: {
+      id: 'id'
+    }
+  },
+  {
+    path: '/bios',
+    name: 'Bios',
+    component: () => import('../views/Bios.vue'),
+  },
+  {
+    path: '/books',
+    name: 'Books',
+    component: () => import('../views/Books.vue'),
+  },
+  {
+    path: '/maintenance',
+    name: 'Maintenance',
+    component: () => import('../views/Maintenance.vue'),
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
