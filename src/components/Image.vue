f73e25023884aa334434e15011658d20ac3e3ce3
Paul Butler III <paul@heysparkbox.com>
2021-09-20 01:58:07 -0600

    feat: create Image component

---
+++ src/components/Image.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="c-image" :src="src" :alt="alt" :style="imageStyle" />
+</template>
+
+<script>
+export default {
+  name: 'Image-Component',
+  props: {
+    src: {
+      type: String,
+      default: '',
+      required: true
+    },
+    alt: {
+      type: String,
+      default: '',
+      required: true
+    },
+    imageStyle: {
+      type: Object,
+      default: () => ({}),
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.c-image {
+  align-self: center;
+}
+</style>
