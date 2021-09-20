45865edb36b7b1eddcf627461a64ffc3180fb211
Paul Butler III <paul@heysparkbox.com>
2021-09-20 02:00:34 -0600

    feat: create Maintenance component for when api is failing

---
+++ src/views/Maintenance.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="c-maintenance">
+    <h1>This be the Maintenance page...aaargh!</h1>
+    <p>It would appear that there has been an error loading the routes of this application</p>
+  </div>
+</template>
