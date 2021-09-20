f018a5db11aed5fda8a5c2041e10baabdac93c83
Paul Butler III <paul@heysparkbox.com>
2021-09-20 02:00:14 -0600

    feat: remove defaults from home and add custom message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home?</h1>
+    <p>I'd recommend using one of the links above...</p>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
